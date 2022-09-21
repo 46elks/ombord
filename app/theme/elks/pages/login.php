5b6738b6016412812ef044895d603ac39567f28c
Eja <eja@46elks.com>
2022-09-21 17:26:34 +0200

    UI: Better display of errors on login and activation page

--- app/theme/elks/pages/login.php
+++ app/theme/elks/pages/login.php
@@ -3,19 +3,21 @@
 // Redirect user to dashboard if already logged in
 if (is_logged_in()) header("Location: /dashboard");
 
+$error = "";
+
 if (isset($_POST['email']) && isset($_POST['password'])):
   
   // Login user  
   $user_obj = ui__api_post("/app/login", [], $_POST['email'], $_POST['password']);
   if($user_obj == null):
-    echo "Felaktiga inloggnigsuppfigter";    
+    $error = "Felaktiga inloggningsuppgifter";    
   else:
     set_session_user_data($user_obj);
     // Redirect to dashboard on sucessfull login
     header("Location: /dashboard");
   endif;
 
-else: ?>
+endif;?>
 
 <?php ui__view_fragment("head.php");?>
 
@@ -23,9 +25,11 @@ else: ?>
   <div class="inner-wrapper">
     <section id="login">
       <h1>Välkommen ombord</h1>
-      <br>
+      <?php if($error): ?>
+        <p class="notice--error"><?=$error;?></p>
+      <?php endif; ?>
       <form action="" method="post">
-        <input type="text" name="email" placeholder="E-postadress" value="">
+        <input type="text" name="email" placeholder="E-postadress" value="<?=(isset($_POST['email'])) ? htmlentities($_POST['email']) : "";?>">
         <input type="password" name="password" value="" placeholder="Lösenord">
         <button type="submit" class="btn">Logga in</button>
       </form>
@@ -34,4 +38,3 @@ else: ?>
 </div>
 
 <?php ui__view_fragment("foot.php");?>
-<?php endif; ?>
